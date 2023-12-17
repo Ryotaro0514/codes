449156ea226cdadfb0d90cbc6413eb1c1b267c47
Ryotaro0514 <149663487+Ryotaro0514@users.noreply.github.com>
2023-12-17 17:52:52 +0900

    Add files via upload

---
+++ Lab_Ise/notice.py
@@ -0,0 +1,8 @@
+import requests
+
+url = "https://notify-api.line.me/api/notify" 
+token = "48Mb58nIDzAqMoAjoxNnMIAgJVNZVIn11JOhd7T86ZN"
+headers = {"Authorization" : "Bearer "+ token} 
+message =  "テスト" 
+payload = {"message" :  message} 
+r = requests.post(url, headers = headers, params=payload)
