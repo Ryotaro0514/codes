32facb45587085daa29896e673ef91f79ee8909b
Ryotaro0514 <149663487+Ryotaro0514@users.noreply.github.com>
2023-12-17 17:16:59 +0900

    Add files via upload

--- Lab_Ise/capture_detect.py
+++ Lab_Ise/capture_detect.py
@@ -12,35 +12,35 @@ def setup():
 def loop():
 	while True:
 		if GPIO.input(PIR_OUT_PIN) == GPIO.LOW:
-          print("0")
-          time.sleep( 1 )
+            print("0")
+            time.sleep( 1 )
 		else:
-          print("1")
+            print("1")
         
-          dt_now = datetime.datetime.now()
-          file_name = dt_now.strftime('%Y年%m月%d日%H時%M分%S秒')
-          print(file_name)
+            dt_now = datetime.datetime.now()
+            file_name = dt_now.strftime('%Y年%m月%d日%H時%M分%S秒')
+            print(file_name)
         
-          #カメラ映像を保存する
-          cap = cv2.VideoCapture(0)
-          fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-          fps = 20.0
-          size = (640, 360)
-          writer = cv2.VideoWriter((file_name + '.m4v'), fmt, fps, size)
+            #カメラ映像を保存する
+            cap = cv2.VideoCapture(0)
+            fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+            fps = 20.0
+            size = (640, 360)
+            writer = cv2.VideoWriter((file_name + '.m4v'), fmt, fps, size)
         
-          i = 0
-          #60フレーム撮影する
-          while i < 60:
-              _, frame = cap.read()
-              frame = cv2.resize(frame, size)
-              writer.write(frame)
+            i = 0
+            #60フレーム撮影する
+            while i < 60:
+                _, frame = cap.read()
+                frame = cv2.resize(frame, size)
+                writer.write(frame)
      
-              i = i + 1
+                i = i + 1
              
-          writer.release()
-          cap.release()
+            writer.release()
+            cap.release()
         
-          time.sleep( 10 )
+            time.sleep( 10 )
 
 def destroy():
 	GPIO.cleanup()                     # Release resource
